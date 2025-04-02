5ccb1561421a3e43a4991015d8dbf94cefda63cd
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-02 10:27:19 +0100

    add new list tag command to the cli entry point

--- gitblend/cli.py
+++ gitblend/cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 import sys
-from gitblend.commands import delete_tag
+from gitblend.commands import delete_tag, list_tags  # Import the new command
 
 
 def main():
@@ -10,9 +10,7 @@ def main():
         prog="gitblend", description="GitBlend - A Git utility tool"
     )
 
-    subparsers = parser.add_subparsers(
-        dest="command", help="Available commands"
-    )
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     # Delete tag command
     delete_tag_parser = subparsers.add_parser(
@@ -22,6 +20,12 @@ def main():
         "tag", type=str, help="The name of the tag to delete"
     )
     delete_tag_parser.set_defaults(func=delete_tag.run)
+
+    # List tags command
+    list_tags_parser = subparsers.add_parser(
+        "list-tags", help="List all Git tags in the repository"
+    )
+    list_tags_parser.set_defaults(func=list_tags.run)
 
     args = parser.parse_args()
 
