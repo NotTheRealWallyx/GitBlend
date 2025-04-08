b34ca55771a9bd17f4d5b92a72e05ef3949571dc
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-08 13:54:10 +0200

    fix reuse of args and add test for the new rename command

--- gitblend/commands/rename_tag.py
+++ gitblend/commands/rename_tag.py
@@ -1,5 +1,6 @@
 import git
 from gitblend.commands import delete_tag, create_tag
+from copy import deepcopy
 
 
 def run(args):
@@ -7,13 +8,15 @@ def run(args):
     new_tag = args.new_tag
 
     try:
-
         print(f"🔄 Renaming tag '{old_tag}' to '{new_tag}'...")
         delete_tag.run(args)
 
-        args.tag = new_tag
-        create_tag.run(args)
+        new_args = deepcopy(args)
+        new_args.tag = new_tag
+
+        create_tag.run(new_args)
 
         print(f"✅ Tag '{old_tag}' has been renamed to '{new_tag}' successfully.")
     except Exception as e:
         print(f"❌ Error while renaming tag '{old_tag}' to '{new_tag}': {e}")
+        raise
