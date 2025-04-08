277c4e0c2d21e6a2fefe31e084db40144309511c
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-08 13:32:30 +0200

    refactor cli and add test for it

--- gitblend/cli.py
+++ gitblend/cli.py
@@ -3,14 +3,7 @@ import sys
 from gitblend.commands import delete_tag, list_tags, create_tag
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="gitblend", description="GitBlend - A Git utility tool"
-    )
-
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
-
-    # Create tag command
+def add_create_tag_command(subparsers):
     create_tag_parser = subparsers.add_parser("create-tag", help="Create a new Git tag")
     create_tag_parser.add_argument(
         "tag", type=str, help="The name of the tag to create"
@@ -25,7 +18,8 @@ def main():
     )
     create_tag_parser.set_defaults(func=create_tag.run)
 
-    # Delete tag command
+
+def add_delete_tag_command(subparsers):
     delete_tag_parser = subparsers.add_parser(
         "delete-tag", help="Delete a git tag locally and remotely"
     )
@@ -34,11 +28,24 @@ def main():
     )
     delete_tag_parser.set_defaults(func=delete_tag.run)
 
-    # List tags command
+
+def add_list_tags_command(subparsers):
     list_tags_parser = subparsers.add_parser(
         "list-tags", help="List all Git tags in the repository"
     )
     list_tags_parser.set_defaults(func=list_tags.run)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="gitblend", description="GitBlend - A Git utility tool"
+    )
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    add_create_tag_command(subparsers)
+    add_delete_tag_command(subparsers)
+    add_list_tags_command(subparsers)
 
     args = parser.parse_args()
 
