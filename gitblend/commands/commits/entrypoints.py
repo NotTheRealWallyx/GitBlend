904e0f3fc527124a0687e9bf2d45ad51613a6a72
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-18 21:07:32 +0200

    rename commit

--- gitblend/commands/commits/entrypoints.py
+++ gitblend/commands/commits/entrypoints.py
@@ -3,7 +3,7 @@ from gitblend.commands.commits import create
 
 def add_create_commit_command(subparsers):
     create_commit_parser = subparsers.add_parser(
-        "create-commit", help="Create a new Git commit with a message"
+        "commit", help="Create a new Git commit with a message"
     )
     create_commit_parser.add_argument(
         "--message", type=str, required=True, help="The commit message"
