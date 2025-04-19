82db16427e0e3e487bb7738aa5d931c99cb15302
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-19 10:28:46 +0200

    add test for the new commit with signature

--- tests/commands/commits/test_create_commit.py
+++ tests/commands/commits/test_create_commit.py
@@ -13,7 +13,7 @@ class TestCreateCommitCommand(unittest.TestCase):
     def test_create_commit_success(self, mock_run):
         mock_run.return_value = MagicMock(returncode=0)
 
-        args = MagicMock(message="Initial commit", add=False)
+        args = MagicMock(message="Initial commit", add=False, sign=False)
 
         with patch("builtins.print") as mock_print:
             run(args)
@@ -48,7 +48,7 @@ class TestCreateCommitCommand(unittest.TestCase):
     def test_create_commit_with_add(self, mock_run):
         mock_run.return_value = MagicMock(returncode=0)
 
-        args = MagicMock(message="Initial commit", add=True)
+        args = MagicMock(message="Initial commit", add=True, sign=False)
 
         with patch("builtins.print") as mock_print:
             run(args)
@@ -58,6 +58,32 @@ class TestCreateCommitCommand(unittest.TestCase):
             )
             mock_run.assert_any_call(
                 [GIT_EXECUTABLE, "commit", "--allow-empty", "-m", "Initial commit"],
+                text=True,
+                check=True,
+            )
+            mock_print.assert_any_call("✅ All files added to the commit.")
+            mock_print.assert_any_call(
+                "✅ Commit created successfully with message: 'Initial commit'"
+            )
+
+    @patch("subprocess.run")
+    def test_create_commit_with_sign(self, mock_run):
+        mock_run.return_value = MagicMock(returncode=0)
+
+        args = MagicMock(message="Initial commit", sign=True)
+
+        with patch("builtins.print") as mock_print:
+            run(args)
+
+            mock_run.assert_any_call(
+                [
+                    GIT_EXECUTABLE,
+                    "commit",
+                    "--allow-empty",
+                    "-m",
+                    "Initial commit",
+                    "--gpg-sign",
+                ],
                 text=True,
                 check=True,
             )
