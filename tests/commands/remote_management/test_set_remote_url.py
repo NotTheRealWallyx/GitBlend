8c5d214c00384a680c56ae9df31f08ca48ca6d54
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-06-04 12:24:23 +0100

    remove unused imports

--- tests/commands/remote_management/test_set_remote_url.py
+++ tests/commands/remote_management/test_set_remote_url.py
@@ -1,7 +1,5 @@
 from unittest.mock import MagicMock, patch
 
-import pytest
-
 from gitblend.commands.remote_management import set_remote_url
 
 
--- tests/commands/remote_management/test_show_remotes.py
+++ tests/commands/remote_management/test_show_remotes.py
@@ -1,7 +1,5 @@
 from unittest.mock import MagicMock, patch
 
-import pytest
-
 from gitblend.commands.remote_management import show_remotes
 
 
