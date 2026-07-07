01c08526ab0c13dcde97e5e8fd69d3b6b0781570
Mikel Sanchez <mikelsanchezmart@gmail.com>
2026-07-07 13:52:35 +0200

    update version test

--- tests/commands/test_version.py
+++ tests/commands/test_version.py
@@ -1,14 +1,10 @@
-from unittest.mock import mock_open, patch
+from unittest.mock import patch
 
 from gitblend.commands import version
 
 
 def test_version_run():
-    mock_pyproject_content = """[tool.poetry]
-name = "gitblend"
-version = "0.1.0"
-"""
-    with patch("builtins.open", mock_open(read_data=mock_pyproject_content)):
+    with patch("gitblend.commands.version.get_version", return_value="0.1.0"):
         with patch("builtins.print") as mock_print:
             version.run(None)
             mock_print.assert_called_once_with("GitBlend version 0.1.0")
